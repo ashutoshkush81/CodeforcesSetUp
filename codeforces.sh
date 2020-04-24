29f52cc4691fd4c3f1d75f48201af11814253854
ASHUTOSH KUSHWAHA <44970367+ashutoshkush81@users.noreply.github.com>
2020-04-24 11:41:23 +0530

    error is fixed

--- codeforces.sh
+++ codeforces.sh
@@ -13,6 +13,7 @@ chr() {
 
 #Go to your directory where u want to create new folder for contest
 mkdir $name_of_folder
+cd $name_of_folder
 
 # On your favorite code editor.
 # I'm using vscode as my default editor
@@ -20,14 +21,13 @@ code -n .
 
 for (( i=0; i<$no_of_file; i++))
 do
-        file_name=$(expr "$i" + 1).cpp
-        touch $file_name
-        echo "//   https://codeforces.com/contest/$2/problem/$(chr $(expr "$i" + 65))"  >> $file_name
-        # Place your default code as you want
-        code $file_name
+	file_name=$(expr "$i" + 1).cpp
+	touch $file_name
+	echo "//   https://codeforces.com/contest/$2/problem/$(chr $(expr "$i" + 65))"	>> $file_name
+	# Place your default code as you want
+	code $file_name
 done
 
 clear
 echo "Welcome to codeforces"
 echo "Good luck and High Rating"
-cd $name_of_folder
